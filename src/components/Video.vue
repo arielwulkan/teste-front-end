e205128bca171f4d9f5110793d3d5ece1286940c
ariel <ariel.wulkan@gmail.com>
2019-11-01 17:20:51 -0300

    Add icons to Video

--- src/components/Video.vue
+++ src/components/Video.vue
@@ -1,20 +1,26 @@
 <template>
   <div class="container">
-    <button class="btn" @click="back()">Voltar</button>
-    <div class="justify-content-md-center mt-5">
-      <div class="col-md-6 text-centered">
-
-          <div v-bind:key="video.id.videoId" v-for="video in videos">
-            
-            <img :src="video.snippet.thumbnails.medium.url" alt="YouTube thumbnail">
+    <button class="btn btn-outline-secondary mt-2" @click="back()"><i class="fas fa-chevron-left"></i></button>
+    <div class="col-md-6 mr-auto ml-auto">
+        <div v-bind:key="video.id.videoId" v-for="video in videos">
+          <h6 class="mb-3">{{ video.snippet.title }}</h6>
+          <img :src="video.snippet.thumbnails.medium.url" alt="YouTube thumbnail">
+          <div class="d-flex justify-content-between mt-3">
             <p>{{video.snippet.channelTitle}}</p>
-            <p>Likes: {{video.statistics.likeCount}}</p>
-            <p>Diskes: {{video.statistics.dislikeCount}}</p>
-            <p>Descrição: {{video.snippet.description}}</p>
-            <p>Views: {{video.statistics.viewCount}}</p>
+            <div class="d-flex">
+              <p class="mr-2">
+                <i class="fas fa-thumbs-up"></i>
+                {{video.statistics.likeCount}}
+              </p>
+              <p>
+                <i class="fas fa-thumbs-down"></i>
+                {{video.statistics.dislikeCount}}
+              </p>
+            </div>
           </div>
-
-      </div>
+          <p>Descrição: {{video.snippet.description}}</p>
+          <p><i class="fas fa-eye"></i> {{video.statistics.viewCount}}</p>
+        </div>
     </div>
   </div>
 </template>
