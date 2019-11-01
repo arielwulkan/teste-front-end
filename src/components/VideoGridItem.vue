0794639fbfce07157e226725b84abedf011e5dd0
ariel <ariel.wulkan@gmail.com>
2019-11-01 16:54:40 -0300

    Add buttom to card item

--- src/components/VideoGridItem.vue
+++ src/components/VideoGridItem.vue
@@ -1,14 +1,15 @@
 <template>
   <div>
-    <a :href="'https://www.youtube.com/watch?v=' + video.id.videoId" target="_blank">
-      <img class="card-img-top" :src="video.snippet.thumbnails.medium.url" alt="YouTube thumbnail">
-    </a>
+    <img class="card-img-top" :src="video.snippet.thumbnails.medium.url" alt="YouTube thumbnail">
     <div class="card-body">
       <h5 class="card-title">{{ video.snippet.title }}</h5>
       <h6
         class="card-subtitle mb-2 text-muted"
-      >{{ video.snippet.channelTitle }} | {{ video.snippet.publishedAt | formatDate }}</h6>
+      >{{ video.snippet.channelTitle }}</h6>
       <p class="card-text">{{ video.snippet.description }}</p>
+      <router-link :to="{name: 'video', params: {id: video.id.videoId}}">
+        <button class="btn btn-outline-info">DETALHES DO VÍDEO</button>
+      </router-link>
     </div>
   </div>
 </template>
