3c683ca967166af39428ca6b23e6b4585d40e087
ariel <ariel.wulkan@gmail.com>
2019-11-02 18:15:02 -0300

    Remove list view

--- src/components/SearchResults.vue
+++ src/components/SearchResults.vue
@@ -4,49 +4,25 @@
       <div class="mr-auto">
         <h3>Resultados para "{{ reformattedSearchString }}"</h3>
       </div>
-      <div class="btn-group ml-auto" role="group">
-        <button
-          @click="changeDisplayMode('grid')"
-          type="button"
-          class="btn btn-outline-secondary"
-          v-bind:class="{ active: displayMode === 'grid' }"
-        >
-          <i class="fas fa-th"></i>
-        </button>
-        <button
-          @click="changeDisplayMode('list')"
-          type="button"
-          class="btn btn-outline-secondary"
-          v-bind:class="{ active: displayMode === 'list' }"
-        >
-          <i class="fas fa-list"></i>
-        </button>
-      </div>
     </div>
 
-    <div class="row" v-if="displayMode === 'grid'">
+    <div class="row">
       <div class="col-md-4 mb-4" v-bind:key="video.id.videoId" v-for="video in videos">
         <div class="card h-100">
           <VideoGridItem v-bind:video="video"/>
         </div>
       </div>
     </div>
-    <div v-else>
-      <div class="card mb-2" v-bind:key="video.id.videoId" v-for="video in videos">
-        <VideoListItem v-bind:video="video"/>
-      </div>
-    </div>
+
   </div>
 </template>
 
 <script>
-import VideoListItem from './VideoListItem';
 import VideoGridItem from './VideoGridItem';
 
 export default {
   name: 'SearchResults',
   components: {
-    VideoListItem,
     VideoGridItem
   },
   data() {
