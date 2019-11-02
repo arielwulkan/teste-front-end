d394a1897530c821d27ce6c731b1d0cbd5141a6a
ariel <ariel.wulkan@gmail.com>
2019-11-02 18:15:02 -0300

    Remove list view

--- src/components/NoResults.vue
+++ src/components/NoResults.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="text-center">
     <i class="far fa-frown fa-5x mb-3"></i>
-    <p>Não encontramos vídeos com o termo buscado</p>
-    <p>Utlilize outras palavras chave</p>
+    <p>Não encontramos vídeos com o termo buscado.</p>
+    <p>Utlilize outras palavras chave.</p>
   </div>
 </template>
 
