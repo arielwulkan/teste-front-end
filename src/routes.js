9433646189e568a6ee990112a0398bdc3746f506
ariel <ariel.wulkan@gmail.com>
2019-11-02 18:24:12 -0300

    Remove unused VideoDetails component

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,9 @@
 import Home from './components/Home.vue';
 import Video from './components/Video.vue';
-import VideoDetails from './components/VideoDetails.vue';
 
 const routes = [
     { path: '/', component: Home, name: 'home', meta: { keepAlive: true } },
     { path: '/video/:id', component: Video, name: 'video', meta: { keepAlive: false } },
-    { path: '/video_details/:id', component: VideoDetails }
 ];
 
 export default routes;
