2887235fd45ce6a960b45460714758249fb0e0fc
ariel <ariel.wulkan@gmail.com>
2019-11-02 18:06:52 -0300

    Translate

--- src/components/SearchResults.vue
+++ src/components/SearchResults.vue
@@ -2,7 +2,7 @@
   <div class="container mb-3">
     <div class="d-flex mb-3">
       <div class="mr-auto">
-        <h3>Search Results for "{{ reformattedSearchString }}"</h3>
+        <h3>Resultados para "{{ reformattedSearchString }}"</h3>
       </div>
       <div class="btn-group ml-auto" role="group">
         <button
