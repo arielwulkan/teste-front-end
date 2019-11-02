be82031302192975dae03e5bb8c8c6f922062507
ariel <ariel.wulkan@gmail.com>
2019-11-02 16:23:39 -0300

    Add NoResults feature

---
+++ src/components/NoResults.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="text-center">
+    <i class="far fa-frown fa-5x mb-3"></i>
+    <p>Não encontramos vídeos com o termo buscado</p>
+    <p>Utlilize outras palavras chave</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NoResults',
+};
+</script>
